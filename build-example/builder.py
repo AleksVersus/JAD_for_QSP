f03f3fefa2dc17c63f7d1f068e03c45aba19e631
AleksVersus <aleksversus@mail.ru>
2021-03-06 00:37:30 +0500

    create main file of builder

---
+++ build-example/builder.py
@@ -0,0 +1,22 @@
+import os, json #импортируем нужные модули
+
+# открываем файл через обёртку with
+with open("project.json","r",encoding="utf-8") as project_file:
+    root=json.load(project_file) # получаем структуру json-файла
+    # root["project"] — список инструкций. Перебираем список инструкций:
+    for instruction in root["project"]:
+        # каждая инструкция представляет собой словарь типа
+        # "build": {словарь} или "start":"startgame.qsp"
+        # получаем инструкцию:
+        this_instruction=list(instruction.keys())[0]
+        if this_instruction=="build":
+            # если инструкция должна собирать файлы
+            # значит она представляет собой словарь
+            build=instruction["build"]
+            # Получаем список ключей
+            build_list=list(build)
+            # проверяем, существует ли ключ "folders"
+            if "folders" in build_list:
+                # если ключ существует, значит мы снова имеем дело со списком:
+                print (build["folders"])
+            
