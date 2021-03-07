bf2bd20a02f4eccb16d080fda0208fb489afccf4
AleksVersus <aleksversus@mail.ru>
2021-03-07 21:20:08 +0500

    Переписан allpath через функцию .walk()

--- build-example/allpath.py
+++ build-example/allpath.py
@@ -1,24 +1,12 @@
 import os, json
 
-build_list=[] # создаём список файлов, из которых потом будем билдить общий txt2gam
-# получаем список файлов в текущей папке
-all_files=os.listdir(".") # точка в данном случае вполне работает, как указатель на текущую папку
-# перебираем все пути в списке
-while len(all_files)>0:
-	print(all_files[0]),
-	if os.path.isdir(all_files[0])==True:
-		print ("- is folder")
-		# если путь является папкой:
-		targfold_files=os.listdir(all_files[0]) # получаем список файлов в указанной папке
-		all_files.extend(targfold_files) # добавляем полученный список к исходному
-		del targfold_files # удаляем полученный список
-	else:
-		print ("- is file")
-		sp=os.path.splitext(all_files[0])
-		# если путь является файлом
-		if (sp[1]==".qsps") or (sp[1]==".qsp-txt") or (sp[1]==".txt-qsp"):
-			print (all_files[0], "< add in build_list")
-			build_list.append(all_files[0])
-	del all_files[0]
-print (all_files)
-print (build_list)
+build_files=[]
+current_folder=os.getcwd()
+tree=os.walk(current_folder)
+for abs_path, folders, files in tree:
+	for file in files:
+		sp=os.path.splitext(file)
+		if sp[1]==".qsps" or sp[1]=='.qsp-txt' or sp[1]=='.txt-qsp':
+			build_files.append(abs_path+'\\'+file)
+for i in build_files:
+	print (i)
