c4b7f6ffffb463ae5caf09e0835179e909fb1f41
AleksVersus <aleksversus@mail.ru>
2021-03-07 11:42:38 +0500

    Добавил функционал в перебиратель путей

--- build-example/allpath.py
+++ build-example/allpath.py
@@ -1,5 +1,24 @@
 import os, json
 
+build_list=[] # создаём список файлов, из которых потом будем билдить общий txt2gam
 # получаем список файлов в текущей папке
-curfold_files=os.listdir(".") # точка в данном случае вполне работает, как указатель на текущую папку
-print (curfold_files)
+all_files=os.listdir(".") # точка в данном случае вполне работает, как указатель на текущую папку
+# перебираем все пути в списке
+while len(all_files)>0:
+	print(all_files[0]),
+	if os.path.isdir(all_files[0])==True:
+		print ("this folder")
+		# если путь является папкой:
+		targfold_files=os.listdir(all_files[0]) # получаем список файлов в указанной папке
+		all_files.extend(targfold_files) # добавляем полученный список к исходному
+		del targfold_files
+	else:
+		sp=os.path.splitext(all_files[0])
+		# если путь является файлом
+		if (sp[1]==".qsps") or (sp[1]==".qsp-txt") or (sp[1]==".txt-qsp"):
+			print (all_files[0], "< add in build_list")
+			build_list.append(all_files[0])
+			break
+	del all_files[0]
+print (all_files)
+print (build_list)
