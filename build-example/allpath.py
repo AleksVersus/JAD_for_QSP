76126daa75c4bdf4f81dca321dc097920e9b1e35
AleksVersus <aleksversus@mail.ru>
2021-03-07 20:34:01 +0500

    модифицировал поиск всех файлов до рабочей версии. Файлы находит!

--- build-example/allpath.py
+++ build-example/allpath.py
@@ -7,18 +7,18 @@ all_files=os.listdir(".") # точка в данном случае вполне
 while len(all_files)>0:
 	print(all_files[0]),
 	if os.path.isdir(all_files[0])==True:
-		print ("this folder")
+		print ("- is folder")
 		# если путь является папкой:
 		targfold_files=os.listdir(all_files[0]) # получаем список файлов в указанной папке
 		all_files.extend(targfold_files) # добавляем полученный список к исходному
-		del targfold_files
+		del targfold_files # удаляем полученный список
 	else:
+		print ("- is file")
 		sp=os.path.splitext(all_files[0])
 		# если путь является файлом
 		if (sp[1]==".qsps") or (sp[1]==".qsp-txt") or (sp[1]==".txt-qsp"):
 			print (all_files[0], "< add in build_list")
 			build_list.append(all_files[0])
-			break
 	del all_files[0]
 print (all_files)
 print (build_list)
