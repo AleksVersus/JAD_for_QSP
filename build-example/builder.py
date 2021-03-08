045842545809a151c87cb0fce2eabf81f2e5a1f8
AleksVersus <aleksversus@mail.ru>
2021-03-08 22:10:05 +0500

    Допилен парсер инструкции start. Осталось наладить запуск

--- build-example/builder.py
+++ build-example/builder.py
@@ -2,7 +2,10 @@ import os, json, subprocess #импортируем нужные модули
 import allpath # импортируем свой модуль
 
 error_log=[] # список ошибок
+work_dir=os.getcwd() # текущая рабочая папка относительно файла project.json
 txt2gam="D:\\my\\GameDev\\QuestSoftPlayer\\QSP 570 QG 400b\\txt2gam.exe" # путь к txt2gam
+player_exe="D:\\my\\GameDev\\QuestSoftPlayer\\QSP 570 QG 400b\\qspgui.exe"
+exit_files_list=[] # список выходных файлов
 # открываем файл json через обёртку with
 with open("project.json","r",encoding="utf-8") as project_file:
     root=json.load(project_file) # получаем структуру json-файла
@@ -40,12 +43,21 @@ with open("project.json","r",encoding="utf-8") as project_file:
             else:
             # если есть чего собирать, собираем:
                 # путь к выходному файлу ставится относительно рабочей папки
-                exit_txt=os.getcwd()+"\\"+os.path.splitext(game_name)[0]+".txt"
+                exit_txt=work_dir+"\\"+os.path.splitext(game_name)[0]+".txt"
                 # путь к конечному файлу
-                exit_qsp=os.getcwd()+"\\"+game_name
+                exit_qsp=work_dir+"\\"+game_name
                 # собираем текстовый файл
                 allpath.constructFile(build_files,exit_txt)
                 # теперь нужно конвертировать файл в бинарник
                 subprocess.run([txt2gam,exit_txt,exit_qsp])
+                if os.path.isfile(exit_qsp):
+                    exit_files_list.append(exit_qsp)
+                # теперь удаляем промежуточный файл
+                os.remove(exit_txt)
             del path_list
-            
+        if this_instruction=="start":
+            start_file=work_dir+"\\"+instruction["start"]
+            if not start_file in exit_files_list:
+                start_file=exit_files_list[0]
+            print (start_file)
+# после обработки json можно запустить указанный файл в плеере
