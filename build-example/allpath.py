82e107583f0431888f48ca0996cf00a915eeec0d
AleksVersus <aleksversus@mail.ru>
2021-03-11 00:44:37 +0500

    Функция, которая ищет project.json и возвращает путь к его папке

--- build-example/allpath.py
+++ build-example/allpath.py
@@ -25,3 +25,22 @@ def constructFile(build_list,new_file):
 	# необходимо записывать файл в кодировке cp1251, txt2gam версии 0.1.1 понимает лишь её
 	with open(new_file,"w",encoding="cp1251") as file:
 		file.write(text)
+
+# данная функция находит папку проекта или возвращает None
+def searchProject(path):
+	error=path
+	# если путь является файлом, получаем только путь
+	if os.path.isfile(path)==True:
+		path=os.path.split(path)[0]
+	# пока не найден файл проекта
+	while os.path.isfile(path+"\\project.json")==False:
+		if os.path.ismount(path)==True:
+			error_log.append("searchProject: not found 'project.json' file for this project. Prove path '"+error+"'.\n")
+			break
+		path=os.path.split(path)[0]
+	else:
+		return path
+	if len(error_log)>0:
+		with open("errors.log","a",encoding="utf-8") as error_file:
+			for i in error_log:
+				error_file.write(i)
---
+++ build-example/test.py
@@ -0,0 +1,12 @@
+# файл для тестирования python-скриптов
+
+import os, json, subprocess
+
+error_log=[] # список сообщений об ошибках
+
+# в аргументе должен быть передан путь к файлу
+argument3="F:\\+++Просмотрено\\Немезиды"
+argument = "D:\\my\\GameDev\\QuestSoftPlayer\\projects\\JAD\\build-example\\обязательные локации\\[death]\\death.qsps"
+
+
+
