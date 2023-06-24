229f8f157ef11b59789c5ad2c2bca3a7f660be2d
aleksversus <aleksversus@mail.ru>
2023-06-24 14:09:52 +0500

    feat (builder): add commands of converters to plugin

--- QSP.sublime-package/QSP.py
+++ QSP.sublime-package/QSP.py
@@ -6,9 +6,13 @@ import sys, os
 # Importing my modules from qSpy package.
 from .qSpy.function import parse_args
 from .qSpy.builder import BuildQSP
+from .qSpy.qsp_to_qsps import QspToQsps
+from .qSpy.qsps_to_qsp import NewQspsFile
 
 class QspBuildCommand(sublime_plugin.WindowCommand):
-
+	"""
+		QSP-Game Builder. Build and run QSP-game from sources. Need a project.json.
+	"""
 	def run(self, qsp_mode = "--br"):
 		# Default paths to converter and player.
 		converter = "qsps_to_qsp" # buil-in converter. WARNING! Test-mode!!!
@@ -28,3 +32,24 @@ class QspBuildCommand(sublime_plugin.WindowCommand):
 		builder = BuildQSP(args, converter, player)
 		# Run the Builder to work:
 		builder.build_and_run()
+
+class QspToQspsCommand(sublime_plugin.WindowCommand):
+
+	def run(self):
+		argv = self.window.extract_variables()
+		file = argv['file']
+		if argv['file_extension'] == 'qsp':
+			qsp_to_qsps = QspToQsps(args = {'game-file': file})
+			qsp_to_qsps.convert()
+		else:
+			print('Wrong extension of file. Can not convert.')
+
+class QspsToQspCommand(sublime_plugin.WindowCommand):
+
+	def run(self):
+		argv = self.window.extract_variables()
+		if argv['file_extension'] == 'qsps':
+			file = NewQspsFile(input_file = argv['file'])
+			file.convert()
+		else:
+			print('Wrong extension of file. Can not convert.')
